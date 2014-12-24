133044e60e3e1c45987ad04b961d80b0abb4c17f
Luan Nico <luanpotter28@hotmail.com>
2014-12-23 22:37:20 -0200

    jquery.cursor-manager

---
+++ src/jquery.cursor-manager.js
@@ -0,0 +1,54 @@
+(function ($) {
+  "use strict";
+
+  var getCursorPosition = function(jthis) {
+    var el = $(jthis).get(0);
+    var pos = 0;
+    if ("selectionStart" in el) {
+      pos = el.selectionStart;
+    } else if ("selection" in document) {
+      el.focus();
+      var sel = document.selection.createRange();
+      var selLength = document.selection.createRange().text.length;
+      sel.moveStart("character", -el.value.length);
+      pos = sel.text.length - selLength;
+    }
+    return pos;
+  };
+
+  var setCursorPosition = function(jthis, pos, endPos) {
+    endPos = endPos || pos;
+    $(jthis).each(function(index, elem) {
+      if (elem.setSelectionRange) {
+        elem.setSelectionRange(pos, pos);
+      } else if (elem.createTextRange) {
+        var range = elem.createTextRange();
+        range.collapse(true);
+        range.moveEnd("character", pos);
+        range.moveStart("character", endPos);
+        range.select();
+      }
+    });
+    return jthis;
+  };
+
+  $.fn.cursor = function() {
+    var jthis = this;
+    return {
+      get : function() {
+        return getCursorPosition(jthis);
+      },
+      set : function(pos, endPos) {
+        return setCursorPosition(jthis, pos, endPos);
+      }
+    };
+  };
+
+  $.fn.stripOut = function(pos) {
+    var a = this.cursor().get();
+    this.val(this.val().substring(0, pos) + this.val().substring(pos + 1));
+    this.cursor().set(a - 1);
+
+    return this;
+  };
+}(jQuery));
---
+++ test/jquery.cursor-manager.test.js
@@ -0,0 +1,25 @@
+(function ($) {
+  "use strict";
+
+  QUnit.test("get", function(assert) {
+    var input = $("<input>");
+    assert.equal(input.cursor().get(), 0);
+
+    input.val('111');
+    assert.equal(input.cursor().get(), 3);
+
+    input.val('11');
+    assert.equal(input.cursor().get(), 2);
+  });
+
+  /* TODO can't test set because of phantom.js inconsistence */
+
+  QUnit.test("stripOut", function(assert) {
+    var input = $("<input>");
+
+    input.val('my text');
+    input.stripOut(2);
+
+    assert.equal(input.val(), 'mytext');
+  });
+}(jQuery));
